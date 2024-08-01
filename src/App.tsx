f45bec9ce83d58c68a8cb2e646c4ff6116aa2646
n0vella <n0vella@outlook.com>
2024-08-01 11:57:30 +0200

    some app styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import AudioPlayer from "./components/AudioPlayer";
+import LeftMenu from "./components/LeftMenu";
+import TitleBar from "./components/TitleBar";
 
 const App = () => {
 
   return (
-    <div>
-      <AudioPlayer src={import.meta.env.VITE_TEST_SRC} />
+    <div className="bg-zinc-900 w-full h-screen flex flex-col rounded-2xl border-slate-800 border-[1px]">
+      <TitleBar/>
+      <div className="flex justify-start w-full h-full">
+        <LeftMenu/>
+        <div className="flex flex-col w-full h-full text-zinc-50">
+          <div className="flex w-full h-full p-2">
+            MAIN
+          </div>
+          <AudioPlayer src={import.meta.env.VITE_TEST_SRC} className="w-full"/>
+        </div>
+      </div>
     </div>
   );
 };
