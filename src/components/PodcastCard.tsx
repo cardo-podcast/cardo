4f7bb3c14b5cf2916f5af70398742e128c8924d8
n0vella <n0vella@outlook.com>
2024-08-03 12:38:38 +0200

    buttons for favorites in left menu

--- src/components/PodcastCard.tsx
+++ src/components/PodcastCard.tsx
@@ -4,18 +4,18 @@ import * as icons from "../Icons"
 import { useNavigate } from "react-router-dom"
 
 
-function PodcastCard ({result, endSearch}: {result: PodcastData, endSearch: ()=>void}) {
+function PodcastCard ({podcast, callback}: {podcast: PodcastData, callback: ()=>void}) {
   const [imageError, setImageError] = useState(false)
   const navigate = useNavigate()
 
 
   return(
-    <div className="flex bg-zinc-800 hover:bg-zinc-600 cursor-pointer rounded-md h-20 p-2 justify-between gap-4"
+    <div className="flex bg-zinc-700 hover:bg-zinc-600 cursor-pointer rounded-md h-20 p-2 justify-between gap-4"
     onClick={() => {
-        endSearch()
+      callback()
         navigate('/preview', {
           state: {
-            podcast: result
+            podcast: podcast
           }
         })
     }}
@@ -24,14 +24,14 @@ function PodcastCard ({result, endSearch}: {result: PodcastData, endSearch: ()=>
         icons.photo:
         <img
         className="bg-zinc-700 h-full aspect-square rounded-md"
-        src={result.coverUrl}
+        src={podcast.coverUrl}
         onError={() => setImageError(true)}
         />
       }
 
       <div className="flex flex-col text-right">
-        <p className="text-lg">{result.podcastName}</p>
-        <p>{result.artistName}</p>
+        <p className="text-lg">{podcast.podcastName}</p>
+        <p>{podcast.artistName}</p>
       </div>
     </div>
   )
