d561769577977448213ea1acd62e9330d03ebd6b
n0vella <177157984+n0vella@users.noreply.github.com>
2024-08-03 23:37:14 +0200

    db favorites tought useContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,34 @@ import TitleBar from "./components/TitleBar";
 import HomePage from "./pages/HomePage";
 import SearchBar from "./components/SearchBar";
 import PodcastPreview from "./pages/PodcastPreview";
-import { loadDB } from "./DB";
-import { useEffect } from "react";
+import { DBProvider } from "./DB";
 
 
 const App = () => {
-  const {ref:audioRef, play} = useAudioPlayer()
-  
-  useEffect(()=>{
-    loadDB()
-  }, [])
+  const { ref: audioRef, play } = useAudioPlayer()
 
   return (
     <div className="bg-zinc-900 w-full h-screen flex flex-col rounded-2xl border-zinc-600 border-[1px] text-zinc-50 overflow-hidden">
       <TitleBar />
-      <div className="flex justify-start w-full h-full overflow-hidden">
-        <BrowserRouter>
-          <LeftMenu />
-          <div className="flex flex-col w-full h-full">
-            <SearchBar/>
-            <div className="flex h-full overflow-y-auto">
-              <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/preview' element={<PodcastPreview play={play}/>} />
-              </Routes>
+      <DBProvider>
+        <div className="flex justify-start w-full h-full overflow-hidden">
+          <BrowserRouter>
+            <LeftMenu />
+            <div className="flex flex-col w-full h-full">
+              <SearchBar />
+              <div className="flex h-full overflow-y-auto">
+                <Routes>
+                  <Route path='/' element={<HomePage />} />
+                  <Route path='/preview' element={<PodcastPreview play={play} />} />
+                </Routes>
+              </div>
+              {
+                <AudioPlayer audioRef={audioRef} className="w-full min-h-[70px] flex-shrink-0" />
+              }
             </div>
-            {
-              <AudioPlayer audioRef={audioRef} className="w-full min-h-[70px] flex-shrink-0" />
-            }
-          </div>
-        </BrowserRouter>
-      </div>
+          </BrowserRouter>
+        </div>
+      </DBProvider>
     </div>
   );
 };
