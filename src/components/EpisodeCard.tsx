539eb62a50f676736bd796b7f564f30a778aed94
n0vella <n0vella@outlook.com>
2024-08-06 23:25:10 +0200

    settings provider episode list shows date and size

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -6,13 +6,17 @@ import { useIntersectionObserver } from "@uidotdev/usehooks"
 import { secondsToStr } from "../utils"
 import { useDB } from "../DB"
 import ProgressBar from "./ProgressBar"
+import { useSettings } from "../sync/Settings"
+
 
 function EpisodeCard({ episode, podcast, play }: { episode: EpisodeData, podcast: PodcastData, play: () => void }) {
   const [imageSrc, setImageSrc] = useState(episode.coverUrl ?? podcast.coverUrl)
   const [imageError, setImageError] = useState(false)
   const navigate = useNavigate()
   const { history: { getEpisodeState } } = useDB()
+  const { globals: { locale } } = useSettings()
   const [reprState, setReprState] = useState({ position: 0, total: episode.duration, complete: false })
+  const [date, setDate] = useState('')
   const [ref, entry] = useIntersectionObserver({
     threshold: 0,
     root: null,
@@ -21,22 +25,37 @@ function EpisodeCard({ episode, podcast, play }: { episode: EpisodeData, podcast
 
   useEffect(() => {
     if (!entry?.isIntersecting) return
-    
+
     // set cover
     episode.coverUrl = episode.coverUrl ?? podcast.coverUrl
     setImageSrc(episode.coverUrl)
     setImageError(false)
 
+    // set print date
+    const episodeYear = episode.pubDate.getFullYear()
+    const actualYear = new Date().getFullYear()
+
+    setDate(episode.pubDate
+      .toLocaleDateString(locale, {
+        day: 'numeric',
+        month: 'short',
+        year: episodeYear < actualYear ? 'numeric' : undefined
+      }
+      )
+    )
+
     // update reproduction state
     getEpisodeState(episode.src).then(state => {
       if (state === undefined) return
 
-      setReprState({ position: state.position, total: state.total, complete:  state.position === state.total})
+      setReprState({ position: state.position, total: state.total, complete: state.position === state.total })
     })
-  }, [entry?.isIntersecting, episode, podcast.coverUrl, getEpisodeState])
+  }, [entry?.isIntersecting, episode, podcast.coverUrl, getEpisodeState, locale])
+
+
 
   return (
-    <div ref={ref} className={`flex ${reprState.complete? 'text-zinc-500': ''} hover:bg-zinc-800 cursor-pointer rounded-md min-h-20 p-2 justify-between gap-4`}
+    <div ref={ref} className={`flex ${reprState.complete ? 'text-zinc-500' : ''} hover:bg-zinc-800 cursor-pointer rounded-md min-h-20 p-2 justify-between gap-4`}
       onClick={() => navigate('/episode-preview', {
         state: {
           episode: episode,
@@ -59,14 +78,15 @@ function EpisodeCard({ episode, podcast, play }: { episode: EpisodeData, podcast
             }
           </div>
 
-          <div className="flex gap-2 flex-col text-right w-full items-end justify-between">
-            <h2 className="">{episode.title}</h2>
+          <div className="flex flex-col text-right w-full items-end justify-between">
+            <p className="text-sm w-full text-left">{date} - {Math.round(episode.size / 1000000)}MB </p>
+            <h2 className="mb-2">{episode.title}</h2>
             <div className="flex w-full gap-2 justify-end">
               {
                 (reprState.position === 0 ||
-                reprState.complete) ?
-                secondsToStr(reprState.total):
-                <ProgressBar position={reprState.position} total={reprState.total} />
+                  reprState.complete) ?
+                  secondsToStr(reprState.total) :
+                  <ProgressBar position={reprState.position} total={reprState.total} />
               }
               <button className="w-7 p-[2px] aspect-square flex justify-center items-center hover:text-amber-600 border-2 border-zinc-600 rounded-full"
                 onClick={e => {
