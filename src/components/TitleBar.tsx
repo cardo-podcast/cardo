830b2810d9638beca6afc82dda717fe74797bb46
n0vella <177157984+n0vella@users.noreply.github.com>
2024-08-13 16:17:44 +0200

    playing status saved even when closing app

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -2,44 +2,59 @@ import { appWindow } from "@tauri-apps/api/window";
 import * as icons from "../Icons"
 import { useState } from "react";
 import { SyncButton } from "../sync/Nextcloud";
-
+import { usePlayer } from "./AudioPlayer";
+import { confirm } from '@tauri-apps/api/dialog';
 
 function TitleBar() {
   const [windowPinned, setWindowPinned] = useState(false)
   const [maximized, setMaximized] = useState(false)
+  const { onExit: savePlayerStatus } = usePlayer()
+
+
+  const executeBeforeExit = async() => {
+    await savePlayerStatus()
+  }
 
   appWindow.isMaximized().then(m =>
     setMaximized(m)
   )
 
+  appWindow.onCloseRequested(async() => {
+    // when closing from titlebar right click or other os callings
+    await executeBeforeExit()
+  })
+
   return (
     <div className={`bg-zinc-950 flex w-full text-white h-10 justify-between px-2 pt-1 items-center relative stroke-[1.5px]`} data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>
-        <div className='flex w-12 justify-between'>
-          <button onClick={() => {
-            appWindow.setAlwaysOnTop(!windowPinned)
-            setWindowPinned(!windowPinned)
-            }} className='hover:text-amber-500 w-6'>
-              {windowPinned? icons.unpin: icons.pin}
-            </button>
-            <SyncButton/>
-        </div>
+      <div className='flex w-12 justify-between'>
+        <button onClick={() => {
+          appWindow.setAlwaysOnTop(!windowPinned)
+          setWindowPinned(!windowPinned)
+        }} className='hover:text-amber-500 w-6'>
+          {windowPinned ? icons.unpin : icons.pin}
+        </button>
+        <SyncButton />
+      </div>
 
-          <h1 data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>Podland</h1>
+      <h1 data-tauri-drag-region={true} onDragStart={appWindow.startDragging}>Podland</h1>
 
-        <div className='flex justify-between gap-1'>
-          <button onClick={() => appWindow.minimize()} className='hover:text-amber-500 w-6'>
-            {icons.minus}
-          </button>
-          <button onClick={() => {
-            appWindow.toggleMaximize()
-            setMaximized(!maximized)
-          }} className='hover:text-amber-500 w-5'>
-            {maximized? icons.unmaximize: icons.maximize}
-          </button>
-          <button onClick={() => appWindow.close()} className='hover:text-red-500 w-6'>
-            {icons.close}
-          </button>
-        </div>
+      <div className='flex justify-between gap-1'>
+        <button onClick={() => appWindow.minimize()} className='hover:text-amber-500 w-6'>
+          {icons.minus}
+        </button>
+        <button onClick={() => {
+          appWindow.toggleMaximize()
+          setMaximized(!maximized)
+        }} className='hover:text-amber-500 w-5'>
+          {maximized ? icons.unmaximize : icons.maximize}
+        </button>
+        <button onClick={async() => {
+          await executeBeforeExit()
+          appWindow.close()
+        }} className='hover:text-red-500 w-6'>
+          {icons.close}
+        </button>
+      </div>
     </div>
   )
 }
