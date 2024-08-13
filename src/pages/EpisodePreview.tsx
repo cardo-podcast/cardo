5a7142af879776c72385e3e322dcdff387437b89
n0vella <n0vella@outlook.com>
2024-08-13 23:21:50 +0200

    episode position, date and size shown on episode preview

--- src/pages/EpisodePreview.tsx
+++ src/pages/EpisodePreview.tsx
@@ -1,38 +1,62 @@
-import {  useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import * as icons from "../Icons"
 import { EpisodeData } from ".."
 import { useLocation, useNavigate } from "react-router-dom"
 import { usePlayer } from "../components/AudioPlayer"
 import { useDB } from "../DB"
-import { parsePodcastDetails } from "../utils"
+import { parsePodcastDetails, secondsToStr } from "../utils"
 import { useTranslation } from "react-i18next"
+import ProgressBar from "../components/ProgressBar"
+import { useSettings } from "../Settings"
 
 
 function EpisodePreview() {
   const [imageError, setImageError] = useState(false)
   const location = useLocation()
   const episode = location.state.episode as EpisodeData
-  const {play} = usePlayer()
+  const [position, setPosition] = useState(0)
+  const { play, playing, position: playingPosition } = usePlayer()
   const navigate = useNavigate()
-  const {subscriptions: {getSubscription}} = useDB()
-  const {t} = useTranslation()
+  const { subscriptions: { getSubscription } } = useDB()
+  const { t } = useTranslation()
+  const { history: { getEpisodeState } } = useDB()
+  const [{ globals: { locale } },] = useSettings()
 
 
-  const fetchPodcastData = async(episode: EpisodeData) => {
+  const getDate = () => {
+    const episodeYear = episode.pubDate.getFullYear()
+    const actualYear = new Date().getFullYear()
+
+    return episode.pubDate
+      .toLocaleDateString(locale, {
+        day: 'numeric',
+        month: 'short',
+        year: episodeYear < actualYear ? 'numeric' : undefined
+      }
+      )
+  }
+
+
+  const fetchPodcastData = async (episode: EpisodeData) => {
     if (episode.podcast) return
 
     const subscription = await getSubscription(episode.podcastUrl)
 
     if (subscription !== undefined) {
       episode.podcast = subscription
-    }else {
+    } else {
       episode.podcast = await parsePodcastDetails(episode.podcastUrl)
     }
-  } 
+  }
 
   useEffect(() => {
     // asynchronously fetch podcast data to allow loadig podcast page clicking on cover
     fetchPodcastData(episode)
+
+    // set actual position if episode was played before
+    getEpisodeState(episode.src).then(
+      episode => setPosition(episode?.position ?? 0)
+    )
   }, [])
 
   return (
@@ -56,10 +80,20 @@ function EpisodePreview() {
           />
         }
 
-        <div className="flex flex-col gap-2">
-          <h1>{episode.title}</h1>
-          <div className="flex gap-2">
-            <button className="w-6 p-[2px] aspect-square flex justify-center items-center hover:text-amber-600 bg-zinc-700 rounded-full"
+        <div className="flex flex-col gap-2 justify-between p-1 w-full">
+          <div className="flex flex-col">
+            <p className='text-sm text-zinc-400'>{getDate()} - {Math.round(episode.size / 1000000)} MB </p>
+            <h1>{episode.title}</h1>
+          </div>
+          <div className="flex gap-2 justify-end items-center">
+            {
+              (position === 0 || position >= episode.duration) && playing?.src != episode.src ?
+                secondsToStr(episode.duration) :
+                <ProgressBar position={playing?.src == episode.src ? playingPosition : position}
+                  total={episode.duration}
+                  className={{ div: 'h-1', bar: 'rounded', innerBar: 'rounded' }} />
+            }
+            <button className="w-7 p-[2px] aspect-square flex justify-center items-center hover:text-amber-600 bg-zinc-700 rounded-full"
               onClick={() => play(episode)}
             >
               {icons.play}
@@ -68,7 +102,7 @@ function EpisodePreview() {
         </div>
       </div>
       <div className="bg-zinc-800 rounded-md p-3 whitespace-pre-line"
-            dangerouslySetInnerHTML={{__html: episode.description}}/>
+        dangerouslySetInnerHTML={{ __html: episode.description }} />
     </div>
   )
 }
