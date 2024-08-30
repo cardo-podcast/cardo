20957cad9c6986d56212d5e91c29f8cec1546528
n0vella <n0vella@outlook.com>
2024-08-30 22:37:59 +0200

    context menu on left menu subscriptions list

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,14 +1,12 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useDB } from "../engines/DB";
 import { useTranslation } from "react-i18next";
 import { sync } from '../Icons'
-import appIcon from '../../src-tauri/icons/icon.png'
-import { SyntheticEvent } from "react";
+import SubscriptionCard from "./SubscriptionCard";
 
 
 function LeftMenu() {
   const { subscriptions, subscriptionsEpisodes: { updateSubscriptionsFeed, updatingFeeds } } = useDB()
-  const navigate = useNavigate()
   const { t } = useTranslation();
 
   return (
@@ -39,23 +37,9 @@ function LeftMenu() {
       </div>
       <div className="flex flex-col gap-1 overflow-y-auto">
         {
-          subscriptions.subscriptions.map((fav, i) => {
+          subscriptions.subscriptions.map(subscription => {
             return (
-              <div key={i} className="p-1 rounded-md flex gap-2 justify-between cursor-pointer hover:bg-primary-8 hover:pl-2 transition-all"
-                onClick={() => navigate('/preview', {
-                  state: {
-                    podcast: fav
-                  }
-                })}
-              >
-                <img
-                  className="h-10 bg-primary-7 aspect-square rounded-md"
-                  src={fav.coverUrl}
-                  alt=''
-                  onError={(e: SyntheticEvent<HTMLImageElement>) => e.currentTarget.src = appIcon}
-                />
-                <p className=" h-10 text-sm w-full truncate">{fav.podcastName}</p>
-              </div>
+             <SubscriptionCard key={subscription.id} podcast={subscription}/>
             )
           }
           )
