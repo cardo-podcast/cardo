18f4d0c6f9940050fa49d4815e89a9ff34686028
MrEricSir <mrericsir@gmail.com>
2025-06-10 16:11:57 -0700

    Add support for fyyd podcast search
    Currently this replaces Apple's search, but it demonstrates how fyyd can find podcasts using Cardo.
    
    Fixes #61

--- src/engines/search/base.ts
+++ src/engines/search/base.ts
@@ -1,7 +1,11 @@
 import { PodcastData } from '../..'
 import { SearchApple } from './apple'
+import { SearchFyyd } from './fyyd'
 
 export async function SearchPodcast(term: string): Promise<Array<PodcastData>> {
   // no more options right now
-  return SearchApple(term)
+
+  // TODO: Figure out how to dedupe results before merging.
+  //return SearchApple(term)
+  return SearchFyyd(term)
 }
