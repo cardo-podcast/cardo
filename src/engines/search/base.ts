2f69cbb5566a126f01081b24984846f653c4d288
MrEricSir <mrericsir@gmail.com>
2025-06-27 00:35:56 -0700

    Add PodcastIndex search
    Currently replaces the Apple/iTunes search with PodcastIndex.
    
    Requires an API key and secret set in the .env file, see podcastindex.ts for setup instructions.
    
    Fixes #32

--- src/engines/search/base.ts
+++ src/engines/search/base.ts
@@ -1,7 +1,9 @@
 import { PodcastData } from '../..'
 import { SearchApple } from './apple'
+import { SearchPodcastIndex } from './podcastindex'
 
 export async function SearchPodcast(term: string): Promise<Array<PodcastData>> {
   // no more options right now
-  return SearchApple(term)
+  //return SearchApple(term)
+  return SearchPodcastIndex(term)
 }
---
+++ src/engines/search/podcastindex.ts
@@ -0,0 +1,54 @@
+import { http } from '@tauri-apps/api'
+import { PodcastData } from '../..'
+import { fetch as tauriFetch } from '@tauri-apps/api/http'
+import { sha1 } from 'js-sha1';
+import tauriConfig from '../../../src-tauri/tauri.conf.json'
+
+// The API key and secret for PodcastIndex must be specified in a .env File. This file is not
+// checked in to git. To build/run/debug you will need to register for the PodcastIndex API
+// at this URL: https://api.podcastindex.org/signup
+
+// Example .env values:
+//     VITE_PODCASTINDEX_API_KEY=abc
+//     VITE_PODCASTINDEX_API_SECRET=123
+
+// Note that these values must be escaped if they contain special characters.
+// The .env values are populated into the Javascript layer by Vite.
+export async function SearchPodcastIndex(term: string): Promise<Array<PodcastData>> {
+  const searchParams = new URLSearchParams({'q': term.trim()});
+  const url = `https://api.podcastindex.org/api/1.0/search/byterm?${searchParams.toString()}`
+  const apiHeaderTime = String(Math.floor(Date.now() / 1000));
+  const apiKey = import.meta.env.VITE_PODCASTINDEX_API_KEY;
+  const apiSecret = import.meta.env.VITE_PODCASTINDEX_API_SECRET;
+  if (!apiKey || !apiSecret) {
+    // Read the comment above if this throws an error.
+    throw new Error("No API key or secret specified for PodcastIndex");
+  }
+
+  const response = await tauriFetch(url, {
+    method: 'GET',
+    responseType: http.ResponseType.JSON,
+    headers: {
+      "Content-Type": "application/json",
+      "X-Auth-Date": apiHeaderTime,
+      "X-Auth-Key": apiKey,
+      "Authorization": sha1(apiKey + apiSecret + apiHeaderTime),
+      "User-Agent": tauriConfig.package.productName + "/" + tauriConfig.package.version
+    }});
+
+  const apiResults = await (response.data as any).feeds;
+  const results = []
+  for (const result of apiResults) {
+    if (result.url) {
+      results.push({
+        podcastName: result.title,
+        artistName: result.author,
+        coverUrl: result.image,
+        coverUrlLarge: result.image,
+        feedUrl: result.url,
+      })
+    }
+  }
+
+  return results
+}
