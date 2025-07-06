d7b684525535f40160337fc7d4081f0b666cccac
cardo-podcast <cardo-podcast@outlook.com>
2025-07-06 16:46:16 +0200

    - returned translations lost in https://github.com/cardo-podcast/cardo/commit/dbd6e94fb9ec2588ff23504ce38b7e0739fac519
    - fixed make translations script so it delete unused keys for all languages after confirmation

--- scripts/makeTranslations.py
+++ scripts/makeTranslations.py
@@ -2,34 +2,53 @@ from deep_translator import GoogleTranslator
 import json
 import os
 
-LOCALE_FOLDER = 'resources/translations'
-SRC = 'es'
-DEST = ['en']
+LOCALE_FOLDER = "resources/translations"
+SRC = "es"
+DEST = ["en"]
 
 
-
-def translate(text, src='ko', dest='en'):
+def translate(text, src="ko", dest="en"):
     translator = GoogleTranslator(source=src, target=dest)
     return translator.translate(text)
 
 
-with open(os.path.join(LOCALE_FOLDER, f'{SRC}.json'), 'r') as f:
+with open(os.path.join(LOCALE_FOLDER, f"{SRC}.json"), "r") as f:
     src = json.load(f)
 
-for destLang in DEST:
-    destFile = os.path.join(LOCALE_FOLDER, f'{destLang}.json')
-    if os.path.exists(destFile):
-        with open(destFile, 'r') as f:
+for dest_lang in DEST:
+    dest_file = os.path.join(LOCALE_FOLDER, f"{dest_lang}.json")
+    if os.path.exists(dest_file):
+        with open(dest_file, "r") as f:
             dest = json.load(f)
     else:
         dest = {}
 
-    for (key, value) in src.items():
-        if not key in dest.keys():
-            dest[key] = translate(value, src=SRC, dest=destLang)
+    for key, value in src.items():
+        if key not in dest.keys():
+            dest[key] = translate(value, src=SRC, dest=dest_lang)
 
-    # delete removed keys from dest
-    dest = {key: value for key, value in dest.items() if key in src}
-
-    with open(destFile, 'w', encoding='utf-8') as f:
+    with open(dest_file, "w", encoding="utf-8") as f:
         dest = json.dump(dest, f, indent=2, ensure_ascii=False)
+
+
+# delete removed keys other languages
+delete_keys = set()
+for lang in os.listdir(LOCALE_FOLDER):
+    lang_file = os.path.join(LOCALE_FOLDER, lang)
+
+    with open(lang_file, "r", encoding="utf-8") as f:
+        translations: dict = json.load(f)
+
+    new_translations = translations.copy()
+    for key, value in translations.items():
+        if key not in src:
+            if key in delete_keys or input(
+                f'Do you want to delete key "{key}" from translations [Y/n]'
+            ) not in ["n", "N"]:
+                delete_keys.add(key)  # avoid redundant questions
+                print(f'Removed key "{key}" from {lang}')
+                new_translations.pop(key)
+
+    if new_translations.keys() != translations.keys():
+        with open(lang_file, "w", encoding="utf-8") as f:
+            json.dump(new_translations, f, indent=2, ensure_ascii=False)
